94ed1f49a3f6cf172651def2836143ba66f91d9a
chasen2021 <50192480+tortooga2@users.noreply.github.com>
2025-04-11 12:06:00 -0700

    Made button bigger

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,8 @@ function App() {
                 >
                     <button
                         style={{
-                            width: "100px",
+                            width: "100%",
+                            height: "100%",
                         }}
                         onClick={() => {
                             setDivWidth(!divWidth);
