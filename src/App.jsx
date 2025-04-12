36a2e3b397a4b761b3c58c0ad4ea8c7c01543510
chasen2021 <50192480+tortooga2@users.noreply.github.com>
2025-04-11 18:09:00 -0700

    Fixed layout to allow for a contant sidebar width

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
                 style={{ borderRadius: "0.5rem" }}
             >
                 <VerticalDiv
-                    width="25%"
+                    width="10%"
                     color="green"
                     style={{
                         display: "flex",
@@ -27,58 +27,60 @@ function App() {
                         borderRadius: "0.5rem",
                     }}
                 ></VerticalDiv>
-                <VerticalDiv
-                    width="100%"
-                    color="lightgreen"
-                    style={{
-                        display: "flex",
-                        alignItems: "center",
-                        marginLeft: "1rem",
-                        borderRadius: "0.5rem",
-                    }}
-                >
-                    <button
+                <HorizontalDiv padding="0rem">
+                    <VerticalDiv
+                        width="100%"
+                        color="lightgreen"
                         style={{
-                            width: "100%",
-                            height: "100%",
-                        }}
-                        onClick={() => {
-                            setDivWidth(!divWidth);
+                            display: "flex",
+                            alignItems: "center",
+                            marginLeft: "1rem",
+                            borderRadius: "0.5rem",
                         }}
                     >
-                        {divWidth ? "Open" : "Close"} Window
-                    </button>
-                </VerticalDiv>
-                <VerticalDiv
-                    before={{
-                        marginLeft: "1rem",
-                        width: "200%",
-                        borderWidth: "1rem",
+                        <button
+                            style={{
+                                width: "100%",
+                                height: "100%",
+                            }}
+                            onClick={() => {
+                                setDivWidth(!divWidth);
+                            }}
+                        >
+                            {divWidth ? "Open" : "Close"} Window
+                        </button>
+                    </VerticalDiv>
+                    <VerticalDiv
+                        before={{
+                            marginLeft: "1rem",
+                            width: "200%",
+                            borderWidth: "1rem",
 
-                        transition:
-                            "width 0.5s ease-in-out 0.1s, border-width 0.2s ease-in-out, margin-left 0.1s ease-in-out 0.05s",
-                    }}
-                    after={{
-                        marginLeft: "0rem",
-                        width: "0%",
-                        borderWidth: "0rem",
+                            transition:
+                                "width 0.5s ease-in-out 0.1s, border-width 0.2s ease-in-out, margin-left 0.1s ease-in-out 0.05s",
+                        }}
+                        after={{
+                            marginLeft: "0rem",
+                            width: "0%",
+                            borderWidth: "0rem",
 
-                        transition:
-                            "width 0.5s ease-in-out, border-width 0.2s ease-in-out 0.4s, margin-left 0.1s ease-in-out 0.45s",
-                    }}
-                    playAnimation={divWidth}
-                    color="lightblue"
-                    style={{
-                        overflowX: "hidden",
-                        borderRadius: "0.5rem",
-                    }}
-                >
-                    <h1 style={{ width: "20rem" }}>Vertical Div</h1>
-                    <p style={{ width: "20rem" }}>
-                        This is a vertical div with a width of 50% and a light
-                        blue background color.
-                    </p>
-                </VerticalDiv>
+                            transition:
+                                "width 0.5s ease-in-out, border-width 0.2s ease-in-out 0.4s, margin-left 0.1s ease-in-out 0.45s",
+                        }}
+                        playAnimation={divWidth}
+                        color="lightblue"
+                        style={{
+                            overflowX: "hidden",
+                            borderRadius: "0.5rem",
+                        }}
+                    >
+                        <h1 style={{ width: "20rem" }}>Vertical Div</h1>
+                        <p style={{ width: "20rem" }}>
+                            This is a vertical div with a width of 50% and a
+                            light blue background color.
+                        </p>
+                    </VerticalDiv>
+                </HorizontalDiv>
             </HorizontalDiv>
         </NewPage>
     );
